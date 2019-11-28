31e875a27042a877e2736d555df631242404f656
Ben Greer <bngreer@gmail.com>
2019-11-28 13:47:04 -0800

    volume tweaks

--- src/Controller.py
+++ src/Controller.py
@@ -90,9 +90,9 @@ class Controller(QObject):
             self.sound.stop()
             self.sound.setVolume(1)
             self.sound.setSource(QUrl.fromLocalFile(file))
-            self.sound.play()
             self.sound.setVolume(1)
-            self.sound.setVolume(self._volume / 100)
+            QTimer.singleShot(1, lambda : self.sound.play())
+            QTimer.singleShot(10, lambda : self.sound.setVolume(self._volume / 100))
 
     @Slot()
     def toggle(self):
